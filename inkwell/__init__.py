dd664a0ea2966b59fe4295916fab1a1b073111fc
diptanu <diptanuc@gmail.com>
2024-09-29 04:04:38 +0000

    re-export symbols

--- inkwell/__init__.py
+++ inkwell/__init__.py
@@ -1,3 +1,18 @@
 from inkwell.utils.logging.logging_config import setup_logging
 
+from inkwell.pipeline import Pipeline
+from inkwell.components import Page, Image, TextBox, Table, Layout, Document, PageFragment, PageFragmentType
+
 setup_logging()
+
+__all__ = [
+    "Pipeline",
+    "Page",
+    "Image",
+    "TextBox",
+    "Table",
+    "Layout",
+    "Document",
+    "PageFragment",
+    "PageFragmentType",
+]
