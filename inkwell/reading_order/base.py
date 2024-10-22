e570fcf0d7a91aa0281eb8730b7472d9c3f8ecdd
Mohit Raghavendra <mohithmitra@gmail.com>
2024-10-22 05:46:10 +0000

    reading order fix

--- inkwell/reading_order/base.py
+++ inkwell/reading_order/base.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 from inkwell.components import Layout
 
 
 class BaseReadingOrderDetector(ABC):
     @abstractmethod
-    def process(self, layout: list[Layout]) -> list[Layout]:
+    def process(
+        self, image_batch: list[np.ndarray], layout_batch: list[Layout]
+    ) -> list[Layout]:
         pass
 
     @property
