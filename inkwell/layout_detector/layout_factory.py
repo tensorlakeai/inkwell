ff761f0579c0e3345b8263a922d4c85a17b5d658
Diptanu Choudhury <diptanuc@gmail.com>
2024-10-23 22:11:34 +0000

    update inkwell to not expose detectron2 directly

--- inkwell/layout_detector/layout_factory.py
+++ inkwell/layout_detector/layout_factory.py
@@ -1,11 +1,7 @@
 # flake8: noqa: E501
 
 from inkwell.layout_detector.base import BaseLayoutDetector
-from inkwell.layout_detector.faster_rcnn_detector import (
-    FasterRCNNLayoutDetector,
-)
 from inkwell.layout_detector.layout_detector import LayoutDetectorType
-from inkwell.layout_detector.layoutlmv3_detector import LayoutLMv3Detector
 from inkwell.utils.env_utils import is_paddleocr_available
 
 
@@ -22,6 +18,10 @@ class LayoutDetectorFactory:
         """
         Get a layout detector based on the type of layout detection engine.
         """
+        from inkwell.layout_detector.faster_rcnn_detector import (
+            FasterRCNNLayoutDetector,
+        )
+        from inkwell.layout_detector.layoutlmv3_detector import LayoutLMv3Detector
         if layout_detector_type == LayoutDetectorType.FASTER_RCNN:
             return FasterRCNNLayoutDetector(**kwargs)
         if layout_detector_type == LayoutDetectorType.LAYOUTLMV3:
